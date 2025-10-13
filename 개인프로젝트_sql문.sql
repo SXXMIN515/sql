80770ea0ca3f9c42f7f92dc2c8249e510f27f080
sxxmin515 <ksb327d2@gmail.com>
2025-10-13 17:44:39 +0900

    251013

--- 개인프로젝트_sql문.sql
+++ 개인프로젝트_sql문.sql
@@ -38,3 +38,27 @@ WHERE member_id = 11;
 
 SELECT DISTINCT category
 FROM class;
+
+SELECT *
+      FROM (
+        SELECT c.class_id
+              ,c.category
+              ,c.class_name
+              ,TO_CHAR(c.registration_start, 'YYYY/MM/DD') AS registration_start
+              ,TO_CHAR(c.registration_end, 'YYYY/MM/DD') AS registration_end
+              ,TO_CHAR(c.start_date, 'YYYY/MM/DD') AS start_date
+              ,TO_CHAR(c.end_date, 'YYYY/MM/DD') AS end_date
+              ,COUNT(e.enrollment_id) OVER(PARTITION BY c.class_id) AS enrollment_count
+              ,c.class_capacity
+              ,ROW_NUMBER() OVER(ORDER BY ${orderBy}) AS rn
+        FROM class c
+        LEFT JOIN enrollment e
+        ON c.class_id = e.class_id
+        ${whereClause}
+      )
+      WHERE rn > :offset AND rn <= :offset + :perPage;
+    
+-- 수업 신청 목록
+SELECT *
+FROM enrollment
+WHERE member_id = 12;
