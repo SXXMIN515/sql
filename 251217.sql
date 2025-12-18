af0fb380c264121c979a74a626c8e4ed7b1f2350
sxxmin515 <ksb327d2@gmail.com>
2025-12-18 17:54:00 +0900

    251218

--- 251217.sql
+++ 251217.sql
@@ -1309,3 +1309,19 @@ BEGIN
     TO_CHAR(v_emp_sal, '$999,999.00'));
 END;
 /
+
+CREATE OR REPLACE PROCEDURE format_phone
+  (p_phone_no IN OUT VARCHAR2) 
+IS
+BEGIN
+  p_phone_no := '(' || SUBSTR(p_phone_no,1,3) ||
+                ')' || SUBSTR(p_phone_no,4,3) ||
+                '-' || SUBSTR(p_phone_no,7);
+END format_phone;
+/
+
+VARIABLE b_phone_no VARCHAR2(15);
+EXECUTE :b_phone_no := '0534212460';
+PRINT b_phone_no;
+EXECUTE format_phone (:b_phone_no);
+PRINT b_phone_no;
